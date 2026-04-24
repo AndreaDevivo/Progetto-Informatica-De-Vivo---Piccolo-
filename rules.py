9981be93621b8fc180c30ffb0cefcc9df6ce92f6
Simone <simone.piccolo@jcmaxwell.it>
2026-04-24 11:24:37 +0200

    rules.py
    Feat: "implementate regole di gioco e modello dati"

---
+++ rules.py
@@ -0,0 +1,23 @@
+def is_even(number:int) -> bool:
+    """Restituisce TRUE se il numero è pari"""           
+    return number % 2 == 0     
+
+def is_vowel(letter:str) -> bool:
+    """Restituisce TRUE se la lettera è una vocale"""
+    return letter.upper() in 'AEIOU'
+
+def compute_expected_answer(position:str, letter:str , number:int) -> bool:
+    """
+    Applica la logica di Brain Shift:
+    TOP -> Numero Pari?
+    BOTTOM -> Vocale? 
+
+    """
+    if position == "TOP":
+        return is_even(number)
+    elif position == "BOTTOM":
+        return is_vowel(letter)
+    else:
+        raise ValueError("Posizione Non valida: Deve essere 'TOP' o 'BOTTOM' ")
+
+
