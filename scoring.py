1ab41df3f5883dc326b1f8a90b934928264c6492
AndreaDevivo <andrea.devivo2008@gmail.com>
2026-05-08 10:12:45 +0200

    creazione file punteggio
    Implement apply_answer function to update score based on correctness.

---
+++ scoring.py
@@ -0,0 +1,8 @@
+def apply_answer(score: int, is_correct: bool) -> int:
+  "Aggiorna il punteggio attuale"
+  
+  if is_correct
+    return score + 10 "Se è corretto prendo 10 punti"
+                      "Se è errato prendo 0 punti"
+
+return score
