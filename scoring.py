863cce6b10b17c1239d2dadbef65ee832bcac777
Simone <simone.piccolo@jcmaxwell.it>
2026-05-16 13:56:48 +0200

    fix di errori di sintassi

--- scoring.py
+++ scoring.py
@@ -1,8 +1,6 @@
 def apply_answer(score: int, is_correct: bool) -> int:
   "Aggiorna il punteggio attuale"
   
-  if is_correct
-    return score + 10 "Se è corretto prendo 10 punti"
-                      "Se è errato prendo 0 punti"
-
-return score
+  if is_correct:
+    return score + 10  # Se è corretto prendo 10 punti
+  return score  # Se è errato prendo 0 punti
